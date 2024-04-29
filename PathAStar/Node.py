ccf9efc05390a6eca1f14d3a6b386d5b7b68f5c6
Jacob Phillips <jpwkjacob@hotmail.com>
2024-04-29 11:52:21 +0200

    Changes to the Node system, and added Neighbours, and edges

--- PathAStar/Node.py
+++ PathAStar/Node.py
@@ -1,11 +1,55 @@
 class Node:
-    def __init__(self, latitude : str, longitude : str, node_id=None):
+    """
+        A node class for A* Pathfinding
+        g is cost from start to current Node
+        h is heuristic based estimated cost for current Node to end Node
+        f is total cost of present node i.e. :  f = g + h + cost
+    """
+
+    def __init__(self, latitude: str, longitude: str, node_id=None, cost=0):
         self.latitude = latitude
         self.longitude = longitude
         self.node_id = node_id
-        self.g = 0
-        self.h = 0
-        self.f = 0
+        self.cost = cost  # Cost or weight associated with traveling to this node
+
+        self.g = 0  # cost from start to current Node
+        self.h = 0  # heuristic based estimated cost for current Node to end Node
+        self.f = 0  # total cost of present node i.e. :  f = g + h + cost
+
+        self.neighbors = []
+        self.degree = 0
+
+    def set_g(self, g):
+        self.g = g
+
+    def set_h(self, h):
+        self.h = h
+
+    def calculate_total_cost(self):
+        """
+        Calculate the total cost f for this node
+        f = g + h + cost
+        """
+        self.f = self.g + self.h + self.cost
+
+    # You can optionally include a set_cost method if you need to update the cost later
+    def set_cost(self, cost):
+        self.cost = cost
+        self.calculate_total_cost()
+
+    def metaNodeData(self):
+        return {
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "node_id": self.node_id,
+            "cost": self.cost,
+            "g": self.g,
+            "h": self.h,
+            "f": self.f,
+            "neighbors": self.neighbors,
+            "degree": self.degree
+        }
 
 
-
+    def __repr__(self):
+        return f"Node({self.node_id}, ({self.latitude}, {self.longitude}), neighbors={[n.node_id for n in self.neighbors]})"
--- PathAStar/PathAStar.py
+++ PathAStar/PathAStar.py
@@ -1,5 +1,6 @@
 import osmium
 import networkx as nx
+from collections import defaultdict
 from Node import Node
 
 class OSMHandler(osmium.SimpleHandler):
@@ -7,36 +8,54 @@ class OSMHandler(osmium.SimpleHandler):
         super(OSMHandler, self).__init__()
         self.graph = nx.Graph()
         self.nodes = {}
+        self.node_connectivity = defaultdict(list)
+
+        self.debug = False
 
     def node(self, n):
-        self.nodes[n.id] = Node(n.location.lat, n.location.lon, n.id)
+        node = Node(n.location.lat, n.location.lon, n.id)
+        self.nodes[n.id] = node
+        self.graph.add_node(node)
+
 
     def way(self, w):
         if 'highway' in w.tags:
-            # Filter out specific road types
-            if w.tags['highway'] in ['primary', 'secondary']:
-                for i in range(len(w.nodes) - 1):
-                    node1 = self.nodes[w.nodes[i].ref]
-                    node2 = self.nodes[w.nodes[i+1].ref]
-                    # Calculate custom weightings
-                    weight = self.calculate_weight(node1, node2)
-                    self.graph.add_edge(node1, node2, weight=weight)
+            road_name = w.tags.get('name', 'Unknown')
+            road_type = w.tags.get('highway', 'Unknown')
+            nodes = [self.nodes[n.ref] for n in w.nodes]
+            edges = list(zip(nodes, nodes[1:]))
+            for n1, n2 in edges:
+                self.graph.add_edge(n1, n2, name=road_name, road_type=road_type)
+                self.node_connectivity[n1].append(n2)
+                self.node_connectivity[n2].append(n1)
+                if self.debug:
+                    print(f"Node {n1.node_id} neighbors: {self.node_connectivity[n1]}")
+                    print(f"Node {n2.node_id} neighbors: {self.node_connectivity[n2]}")
 
-    def calculate_weight(self, node1, node2):
-        # Example: Custom weighting based on distance between nodes
-        distance = self.calculate_distance(node1, node2)
-        # You can define your own custom weighting function here
-        # For example, you could use distance, road type, etc.
-        return distance
+    def post_process(self):
+        for node, neighbors in self.node_connectivity.items():
+            node.neighbors = neighbors
+            node.degree = len(neighbors)
+            if self.debug:
+                print(f"Node {node.node_id} neighbors: {node.neighbors}")
 
-    def calculate_distance(self, node1, node2):
-        # Simple Euclidean distance calculation
-        lat1, lon1 = node1.latitude, node1.longitude
-        lat2, lon2 = node2.latitude, node2.longitude
-        return ((lat1 - lat2)**2 + (lon1 - lon2)**2)**0.5
 
 # Parse the OSM file and create the graph
-osm_file = "your_osm_file.osm"
+osm_file = "../aalborg_map.osm"
 handler = OSMHandler()
 handler.apply_file(osm_file)
 graph = handler.graph
+
+
+# Print the number of nodes and edges
+print("Number of nodes:", graph.number_of_nodes())
+print("Number of edges:", graph.number_of_edges())
+
+
+# print the first 10 nodes
+print("First node:", list(graph.nodes())[:1])
+
+# print the first 10 edges
+print("First edge:", list(graph.edges())[:1])
+
+# Get the node with a specific ID
